9ef22089b57761087c600cee8a222c976c886ee1
Marwan Ghanem <90506494+Marwann11@users.noreply.github.com>
2021-11-09 23:08:01 +0200

    chore: setup get and post routes and requests

--- server.js
+++ server.js
@@ -25,14 +25,28 @@ app.use(express.static('website'));
 // Spin up the server
 const port = 3000;
 
-const server = app.listen(port, () => {
-  console.log('Server is running');
-  console.log(`the server is running on localhost:${port}`)
-});
+const server = app.listen(port, listener);
+
+// server callback
+function listener() {
+  console.log(`server is running at localhost:${port}`);
+}
 
 // Initialize all route with a callback function
+app.get('/all', getProjectData)
 
 // Callback function to complete GET '/all'
+function getProjectData(req,res) {
+  res.send(weatherData);
+};
 
 // Post Route
-  
+app.post('/post', (req,res) => {
+  const newEntry = {
+    temperature: req.body.temperature,
+    date: req.body.date,
+    userResponse: req.body.userResponse
+  };
+
+  weatherData.splice(0,0,newEntry); // add every new entry as the zero index in our object
+})
--- website/app.js
+++ website/app.js
@@ -1,12 +1,52 @@
+// base URL for OpenWeatherMap API
+const baseUrl = 'api.openweathermap.org/data/2.5/weather?zip='
 // Personal API Key for OpenWeatherMap API
+const apiKey = '&appid=11c7cee1165ceb404637d6f6777b30a5';
 
 // Event listener to add function to existing HTML DOM element
+document.getElementById('generate').addEventListener('click', getPostUpdate)
 
 /* Function called by event listener */
+function getPostUpdate(evt) {
+  let zipCode = document.getElementById('zip').value;
+  let feelings = document.getElementById('feelings').value;
+
+  getData(baseUrl,zipCode,apiKey)
+  .then(function(data) {
+    console.log(data); // for debugging
+
+  })
+}
 
 /* Function to GET Web API Data*/
+const getData = async (baseUrl,zipCode,key) => {
+  const request = await fetch(`${baseUrl}${zipCode}${key}`); // the format to get apiData
+
+  try {
+    const data = await request.json();
+    return data; // return data so we can use in our post request
+  } catch(error) {
+    console.log('error',error); // appropriately handle the error
+  }
+}
 
 /* Function to POST data */
+const postData = async (url = '', data = {}) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data) // body data type must match headers Content-Type 
+  })
 
+  try {
+    const newData = await response.json() 
+    return newData;
+  } catch(error) {
+    console.log('error',error); // appropriately handle the error
+  }
+}
 
-/* Function to GET Project Data */
+/* Function to GET Project Data and update UI */
